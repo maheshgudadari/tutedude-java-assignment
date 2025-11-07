05dfd6f72ec19e9ea98f5308f3f0f1b7af93770a
maheshgudadari <maheshgudadari@gmail.com>
2025-11-07 17:49:34 +0530

    assignment 1 is donee

---
+++ assignment1/assignment1/assignment1.java
@@ -0,0 +1,28 @@
+package assignment1;
+
+public class assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=30;
+		//, double, char, boolean, String
+		double b=20;
+		boolean c= true;
+		char d ='t';
+		
+		String str1 = "tute";
+		String str2 = "dude";
+		
+		System.out.println(" numaric operations");
+		System.out.println("addition of number"+(a+b));
+		
+		System.out.println("substratction of number::"+(a-b));
+		System.out.println("multification of number::"+(a*b));
+		System.out.println("Concatenate  of string::"+str1+str2);
+		
+		
+
+	}
+
+}
